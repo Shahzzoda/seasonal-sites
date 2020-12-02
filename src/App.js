ffe949bc33f92e1293e2bdd60e9b0605365f34d7
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-12-01 22:10:56 -0500

    first page iteration 1 complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Happy Holidays, Friend!
-        </p>
-      </header>
+    <div className="first">
+      <h1>Happy Holidays, Friend!</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Confetti from 'react-confetti'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Confetti
+      recycle={false}
+      friction={1}
+      gravity={0.25}
+      numberOfPieces={500}
+      wind={0}
+      tweenDuration={100}
+      colors={['#87eafa', '#9987fa', '#ffffff', '#002798']}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
