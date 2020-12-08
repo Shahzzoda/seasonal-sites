f08723e8d3ccaaf53f327c3c3ee47106dd3e7c4b
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-12-07 21:56:18 -0500

    santa hats falling responsive

--- christmas_vn/src/App.js
+++ christmas_vn/src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <img src="hat.png" className="hat hat-1" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-2" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-3" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-4" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-5" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-6" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-7" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-8" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-9" alt="christmas hat falling" />
-      <img src="hat.png" className="hat hat-10" alt="christmas hat falling" />
+    <div className="container">
+      <div className="hat-rack">
+        <img src="hat.png" className="hat hat-1" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-2" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-3" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-4" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-5" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-6" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-7" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-8" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-9" alt="christmas hat falling" />
+        <img src="hat.png" className="hat hat-10" alt="christmas hat falling" />
+      </div>
+      <div className="intro-words">
+        <h1>Merry Christmas!</h1>
+      </div>
     </div>
   );
 }
