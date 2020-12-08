a1f61aebf8f749987230ccab077fd4d5ac92fff3
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-12-07 23:05:42 -0500

    lights on the side

--- christmas_vn/src/App.js
+++ christmas_vn/src/App.js
@@ -16,8 +16,29 @@ function App() {
         <img src="hat.png" className="hat hat-10" alt="christmas hat falling" />
       </div>
       <div className="intro-words">
-        <h1>Merry Christmas!</h1>
+        <h1>Merry Christmas, Ya Filthy Animal!</h1>
       </div>
+      {/* NOTE: these are temp! and will be replaced by one or two pngs when site is done */}
+      <img src="lights.png" className="lights left lights-1" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-2" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-3" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-4" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-5" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-6" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-7" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-8" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-9" alt="christmas lights" />
+      <img src="lights.png" className="lights left lights-10" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-1" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-2" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-3" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-4" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-5" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-6" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-7" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-8" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-9" alt="christmas lights" />
+      <img src="lights.png" className="lights right lights-10" alt="christmas lights" />
     </div>
   );
 }
