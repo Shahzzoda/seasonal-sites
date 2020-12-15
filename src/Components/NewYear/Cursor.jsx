655482c017a8830ed2a0e46165b787a3de47c9dd
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-12-14 21:04:19 -0500

    keyboard only focus added and aira hidden added to cursor

--- src/Components/NewYear/Cursor.jsx
+++ src/Components/NewYear/Cursor.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Cursor = ({ img, class_name, alt }) => {
   return (
-    <div className={class_name}>
+    <div className={class_name} aria-hidden="true">
       <img src={img} alt={alt} />
     </div>
   )
