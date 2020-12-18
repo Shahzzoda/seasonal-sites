ace59575f42ee43bb3196b17a32ee9131209dcc8
Shahzoda Davlatova <shahzoda01@gmail.com>
2020-12-18 17:25:45 -0500

    form saves data and properly binds to input elements

--- sign-up-form/src/App.js
+++ sign-up-form/src/App.js
@@ -1,28 +1,47 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useInput } from "./input-hook";
+import { intro_words, letter_content, farewell } from "./Data/ChristmasPageData";
+
+
 
 function App() {
+  const { value: openingText, bind: bindOpeningText } = useInput(intro_words.msg)
+  const { value: from, bind: bindFrom } = useInput(intro_words.from)
+  const { value: to, bind: bindTo } = useInput(intro_words.to)
+  const { value: letterGreeting, bind: bindLetterGreeting } = useInput(letter_content.greeting)
+  const { value: letterText, bind: bindLetterText } = useInput(letter_content.msg)
+  const { value: closingText, bind: bindClosingText } = useInput(farewell.msg)
+  const { value: signOff, bind: bindSignOff } = useInput(farewell.from)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("testing data save: ", openingText);
+  }
+
   return (
-    <div className="container">
+    <div className="container" >
       <h1>Fill out the following information</h1>
-      <form action="/action_page.php">
-        <label for="fname">Banner Welcome Text:</label>
-        <input type="text" id="fname" name="fname" value="Merry Christmas, Ya Filthy Animal" />
-        <label for="lname">From:</label>
-        <input type="text" id="lname" name="lname" value="Shahzoda" />
-        <label for="fname">To:</label>
-        <input type="text" id="fname" name="fname" value="Siam" />
-        <label for="lname">Greeting:</label>
-        <input type="text" id="lname" name="lname" value="Dear Siam," />
-        <label for="fname">Letter Text:</label>
-        <input type="text" id="fname" name="fname" value="Lorem Ipsum..." />
-        <label for="lname">Banner Goodbye Text:</label>
-        <input type="text" id="lname" name="lname" value="And a Happy New Year!" />
-        <label for="fname">Sign off:</label>
-        <input type="text" id="fname" name="fname" value="Love, zoda..." />
-        <input type="submit" value="Submit" />
+      <form>
+        <label htmlFor="fname">Banner Opening Text:</label>
+        <input type="text" name="welcomeText" value={openingText} {...bindOpeningText} />
+        <label htmlFor="lname">From:</label>
+        <input type="text" id="lname" name="lname" value={from} {...bindFrom} />
+        <label htmlFor="fname">To:</label>
+        <input type="text" value={to} {...bindTo} />
+        <label htmlFor="lname">Greeting:</label>
+        <input type="text" id="lname" name="lname" value={letterGreeting} {...bindLetterGreeting} />
+        <label htmlFor="fname">Letter Text:</label>
+        <input type="text" value={letterText} {...bindLetterText} />
+        <label htmlFor="lname">Banner Closing Text:</label>
+        <input type="text" id="lname" name="lname" value={closingText} {...bindClosingText} />
+        <label htmlFor="fname">Sign off:</label>
+        <input type="text" value={signOff} {...bindSignOff} />
+        <button value="Submit" onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
+
 }
 
 export default App;
