298a9df889a1b903bd48634cd7de98da5a2d8113
Shahzoda Davlatova <shahzoda01@gmail.com>
2021-04-27 01:59:37 -0400

    db configured for text

--- form/src/App.js
+++ form/src/App.js
@@ -2,8 +2,20 @@ import "./App.css";
 import React from "react";
 import { useInput } from "./input-hook";
 import { intro_words, letter_content, farewell } from "./DefaultData/ChristmasPageData";
+import firebase from "firebase/app"
+
+require("firebase/firestore");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: 'redacted',
+  authDomain: 'redacted',
+  projectId: 'redacted'
+});
 
 function App() {
+  var db = firebase.firestore();
+
   const { value: openingText, bind: bindOpeningText } = useInput(intro_words.msg)
   const { value: from, bind: bindFrom } = useInput(intro_words.from)
   const { value: to, bind: bindTo } = useInput(intro_words.to)
@@ -15,6 +27,17 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("testing data save: ", openingText);
+    db.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    })
+      .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
   }
   return (
     <div className="container" >
