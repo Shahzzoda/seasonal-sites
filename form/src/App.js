217a97524875561e7e442d689ca1eb5ae579e32e
siam132 <shahidullah.rahman223@gmail.com>
2021-04-29 17:40:51 -0400

    Added file upload mechanism, retrives urls and stores in an array to be later put in firebase

--- form/src/App.js
+++ form/src/App.js
@@ -1,46 +1,73 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { useInput } from "./input-hook";
-import { intro_words, letter_content, farewell } from "./DefaultData/ChristmasPageData";
-import firebase from "firebase/app"
-
-require("firebase/firestore");
-
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: 'redacted',
-  authDomain: 'redacted',
-  projectId: 'redacted'
-});
+import {
+  intro_words,
+  letter_content,
+  farewell,
+} from "./DefaultData/ChristmasPageData";
+import firebase from "firebase/app";
 
 function App() {
   var db = firebase.firestore();
+  const storage = firebase.storage().ref("Images");
 
-  const { value: openingText, bind: bindOpeningText } = useInput(intro_words.msg)
-  const { value: from, bind: bindFrom } = useInput(intro_words.from)
-  const { value: to, bind: bindTo } = useInput(intro_words.to)
-  const { value: letterGreeting, bind: bindLetterGreeting } = useInput(letter_content.greeting)
-  const { value: letterText, bind: bindLetterText } = useInput(letter_content.msg)
-  const { value: closingText, bind: bindClosingText } = useInput(farewell.msg)
-  const { value: signOff, bind: bindSignOff } = useInput(farewell.from)
+  const [files, setFiles] = useState(null);
+  const urls = [];
 
-  const handleSubmit = (event) => {
+  const { value: openingText, bind: bindOpeningText } = useInput(
+    intro_words.msg
+  );
+  const { value: from, bind: bindFrom } = useInput(intro_words.from);
+  const { value: to, bind: bindTo } = useInput(intro_words.to);
+  const { value: letterGreeting, bind: bindLetterGreeting } = useInput(
+    letter_content.greeting
+  );
+  const { value: letterText, bind: bindLetterText } = useInput(
+    letter_content.msg
+  );
+  const { value: closingText, bind: bindClosingText } = useInput(farewell.msg);
+  const { value: signOff, bind: bindSignOff } = useInput(farewell.from);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    console.log("Uploading Files to storage bucket...");
+    await handleUpload();
     console.log("testing data save: ", openingText);
-    db.collection("users").add({
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    })
+    db.collection("users")
+      .add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      })
       .then((docRef) => {
         console.log("Document written with ID: ", docRef.id);
       })
       .catch((error) => {
         console.error("Error adding document: ", error);
       });
-  }
+  };
+
+  const handleUpload = async () => {
+    for (let i = 0; i < files.length; i++) {
+      storage
+        .child(`${files[i].name}`)
+        .put(files[i])
+        .then((snapshot) => {
+          console.log(
+            `Uploade Successful!!! \nBytes Transferred:${snapshot.bytesTransferred} `
+          );
+        })
+        .then((res) => {
+          storage
+            .child(files[i].name)
+            .getDownloadURL()
+            .then((res) => urls.push(res));
+        });
+    }
+  };
   return (
-    <div className="container" >
+    <div className="container">
       <h1>Fill out the following information</h1>
       <form>
         {/* TODO: create seperate component to adhere to DRY principles v.v */}
@@ -58,11 +85,11 @@ function App() {
         <input value={closingText} {...bindClosingText} />
         <label>Sign off:</label>
         <input type="text" value={signOff} {...bindSignOff} />
+        <input type="file" onChange={(e) => setFiles(e.target.files)}></input>
         <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
-
 }
 
 export default App;
--- form/src/index.js
+++ form/src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import firebaseConfig from "./firebaseConfig.json";
+import firebase from "firebase/app";
+require("firebase/firestore");
+require("firebase/storage");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
