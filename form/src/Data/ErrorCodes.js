48310df2958082518d3a7b07ec11eee2ef996e5b
Shahzoda Davlatova <shahzoda01@gmail.com>
2021-05-05 15:39:26 -0400

    removing unnecessary error

--- form/src/Data/ErrorCodes.js
+++ form/src/Data/ErrorCodes.js
@@ -4,11 +4,10 @@ const errorDesc = {
     "auth/user-not-found": "Email or password incorrect",
     "auth/user-disabled": "Acount has been disabled. Contact the developers to "
         + "learn more and possibly regain access",
-    "auth/operation-not-allowed": "",
     "auth/popup-blocked": "Popup was blocked by the browser.",
     "auth/popup-closed-by-user": "Popup was closed before completing sign in.",
     "auth/email-already-in-use": "Email address already in use.",
     "auth/weak-password": "Password too weak."
-}
+};
 
-export default errorDesc
+export default errorDesc;
